9f1bd78c845fa265469e5d6c655519c3a3f1ce97
divrawat <divrawat2001@gmail>
2025-06-15 11:24:21 +0530

    wef0wef

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href={`${DOMAIN}/favicon.ico`} sizes="any" />
+        <meta name="google-site-verification" content="6-XtSzr71_G64E3c4EdsVm7VnQEg2oKk7U9CASyxZKA" />
       </Head>
       <body>
         <Main />
