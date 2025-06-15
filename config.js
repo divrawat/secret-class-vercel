a9b9a27fc7674ad49f7203e4335024ce42b89b03
divrawat <divrawat2001@gmail>
2025-06-15 11:20:05 +0530

    wef0ewf

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // export const DOMAIN = 'http://localhost:3000';
-export const DOMAIN = 'https://secretclass.in.net';
+export const DOMAIN = 'https://secretclass-manga.vercel.app';
 
-export const DOMAIN_NAME = 'secretclass.in.net';
+export const DOMAIN_NAME = 'secretclass-manga.vercel.app';
 
 export const IMAGES_SUBDOMAIN = 'https://images.thesololevelingmanga.online/secret-class';
 export const IMAGES_SUBDOMAIN_LOGO = 'https://images.secretclass.in.net';
